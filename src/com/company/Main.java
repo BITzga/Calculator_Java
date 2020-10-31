d5795005da2d4fcbb129df8f23bbaf49f62b17c5
BITzga <45969257+BITzga@users.noreply.github.com>
2020-10-31 15:38:00 +0800

    封装了词法分析

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,22 +10,24 @@ public class Main {
         Tokens MyTokens = new Tokens();
         String expr = "1 ";
 
-        boolean err_check = wordAnalyze(MyTokens.word, expr);
+        WordProcess.getTokens(MyTokens, expr);
+//*****
+        /*boolean err_check = wordAnalyze(MyTokens.word, expr);
         MyTokens.word.add(new Pair(" ",TokenType.EMPTY));
 
         System.out.println(MyTokens);
 
         if (err_check == true) {
             System.out.println("词法正确!");
-        }
-
+        }*/
+//********词法分析
         try {
             Expr exp = getExpr(MyTokens);
             System.out.println(MyTokens.getIndex());
-            System.out.println(MyTokens.word.size()-1);
-            if (MyTokens.getIndex() +1<= MyTokens.word.size()-1) {
+            System.out.println(MyTokens.word.size() - 1);
+            if (MyTokens.getIndex() + 1 <= MyTokens.word.size() - 1) {
 
-                throw new SyntaxException("发现多余字符", MyTokens.getIndex() + 1, MyTokens.word.size()-1);
+                throw new SyntaxException("发现多余字符", MyTokens.getIndex() + 1, MyTokens.word.size() - 1);
 
             } else {
                 System.out.println("语法正确！");
@@ -41,9 +43,8 @@ public class Main {
     }
 
 
-
     public static void trimSpace(Tokens MyTokens) {
-        if (MyTokens.getIndex() > MyTokens.word.size()-1) {
+        if (MyTokens.getIndex() > MyTokens.word.size() - 1) {
             return;
         }
 
@@ -60,7 +61,7 @@ public class Main {
     }
 
     public static boolean Is(Tokens MyTokens, TokenType Text) {
-        if (MyTokens.getIndex() > MyTokens.word.size()-1) {
+        if (MyTokens.getIndex() > MyTokens.word.size() - 1) {
             return false;
         }
 
@@ -81,8 +82,8 @@ public class Main {
     }
 
     public static Expr getNumber(Tokens MyTokens, int sign) {
-        boolean gotFlout=  false;
-        boolean gotInt=false;
+        boolean gotFlout = false;
+        boolean gotInt = false;
         trimSpace(MyTokens);
         Tokens MyTokensBuff;
         MyTokensBuff = (Tokens) MyTokens.clone();
@@ -111,12 +112,11 @@ public class Main {
 
                 MyTokensBuff.PlusIndex();
 
-            }
-            else {
+            } else {
                 break;
             }
-            if(MyTokensBuff.getIndex() == MyTokens.word.size()){
-               // MyTokensBuff.setIndex(MyTokens.word.size()-1);
+            if (MyTokensBuff.getIndex() == MyTokens.word.size()) {
+                // MyTokensBuff.setIndex(MyTokens.word.size()-1);
                 break;
             }
             p = MyTokensBuff.word.get(MyTokensBuff.getIndex());
@@ -171,7 +171,7 @@ public class Main {
                     return Result;
 
                 } else
-                    throw new SyntaxException("此处需要右括号", MyTokensBuff.getIndex(), MyTokensBuff.word.size()-1);
+                    throw new SyntaxException("此处需要右括号", MyTokensBuff.getIndex(), MyTokensBuff.word.size() - 1);
 
             } else {
                 throw e;
@@ -241,105 +241,9 @@ public class Main {
         MyTokens.setIndex(MyTokensBuff.getIndex());
         return Result;
     }
+//词法分析
 
-    public static boolean isCharater(char c) {
-        return c == '(' || c == ')' || c == '+'
-                || c == '-' || c == '*' || c == '/' || c == ' ';
-
-    }
-
-    public static boolean isDigit(char c) {
-
-        return c >= '0' && c <= '9';
-
-    }
-
-    public static boolean wordAnalyze(Vector<Pair> word, String expr) {
-
-        char[] exp = new char[expr.length() + 1];
-        for (int i = 0; i < expr.length(); i++)
-            exp[i] = expr.charAt(i);
-        System.out.println(exp);
-        for (int i = 0; i < expr.length(); i++) {
-            boolean checkFloat = false;
-            // 如果是 + - * / () 存起来 方便后续 中缀转后缀
-            if (isCharater(exp[i])) {
-                StringBuilder tmp = new StringBuilder();
-                tmp.append(exp[i]);
-                switch (exp[i]) {
-                    case ' ':
-                        word.add(new Pair(tmp.toString(), TokenType.SPACE));
-                        break;
-                    case '+':
-                        word.add(new Pair(tmp.toString(), TokenType.PLUS));
-                        break;
-                    case '-':
-                        word.add(new Pair(tmp.toString(), TokenType.MINUS));
-                        break;
-                    case '*':
-                        word.add(new Pair(tmp.toString(), TokenType.MULTI));
-                        break;
-                    case '/':
-                        word.add(new Pair(tmp.toString(), TokenType.DIVIDE));
-                        break;
-                    case '(':
-                        word.add(new Pair(tmp.toString(), TokenType.LEFT));
-                        break;
-                    case ')':
-                        word.add(new Pair(tmp.toString(), TokenType.RIGHT));
-                        break;
-
-                }
-            }
-            //存数字
-            else if (isDigit(exp[i])) {
-
-                StringBuilder tmp = new StringBuilder();
-
-                while (isDigit(exp[i])) {
-                    tmp.append(exp[i]);
-                    ++i;
-                }
-                if (exp[i] == '.') {
-
-                    ++i;
-                    if (isDigit(exp[i])) {
-                        checkFloat = true;
-                        tmp.append('.');
-                        while (isDigit(exp[i])) {
-
-                            tmp.append(exp[i]);
-                            ++i;
-                        }
-                    } else {
-                        TokenException e = new TokenException("小数尾数不完整！",i);
-                        System.out.println(e);
-                        return false;
-                    }
-                }
-                if (checkFloat)
-                    word.add(new Pair(tmp.toString(), TokenType.FLOAT));
-                else {
-                    word.add(new Pair(tmp.toString(), TokenType.INT));
-
-                }
-
-                --i;
-
-            }
-            //以‘.’开头
-            else {
-                TokenException e =  new TokenException("小数前缀不完整！",i);
-                System.out.println(e);
-                return false;
-            }
-
-        }
-        return true;
-    }
-
-    //语法分析
-
+//词法分析
 
 }
 
